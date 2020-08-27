ca31f677b2b66f50a7523b2744a1f4796f784949
Ben Campbell <ben@scumways.com>
2020-08-27 17:33:20 +1200

    summarytool: add option to output data as csv

--- cmd/summarytool/main.go
+++ cmd/summarytool/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/bcampbell/scrapeomat/slurp"
 	"golang.org/x/crypto/ssh/terminal"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -15,6 +17,7 @@ var opts struct {
 	from, to  string
 	pubs      pubArgs
 	termWidth int
+	csv       bool
 }
 
 type pubArgs []string
@@ -27,6 +30,7 @@ func init() {
 	flag.StringVar(&opts.to, "to", "", "to date")
 	flag.IntVar(&opts.termWidth, "w", 0, "output width (0=auto)")
 	flag.StringVar(&opts.server, "s", "http://localhost:12345", "`url` of API server to query")
+	flag.BoolVar(&opts.csv, "c", false, "output as csv rather than ascii-art")
 	flag.Var(&opts.pubs, "p", "publication code(s)")
 
 	flag.Usage = func() {
@@ -43,7 +47,7 @@ func main() {
 
 	flag.Parse()
 
-	if opts.termWidth == 0 {
+	if !opts.csv && opts.termWidth == 0 {
 		var err error
 		opts.termWidth, err = detectTermWidth()
 		if err != nil {
@@ -85,7 +89,15 @@ func main() {
 
 	cooked := slurp.CookSummary(raw)
 
-	dump(cooked, opts.termWidth)
+	if opts.csv {
+		err = dumpCSV(cooked)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		dump(cooked, opts.termWidth)
+	}
 }
 
 func detectTermWidth() (int, error) {
@@ -125,4 +137,30 @@ func dump(cooked *slurp.CookedSummary, termW int) {
 		fmt.Printf("\n")
 	}
 
+}
+
+// Output the summary as a csv file
+func dumpCSV(cooked *slurp.CookedSummary) error {
+
+	out := csv.NewWriter(os.Stdout)
+
+	// header
+	header := []string{"publication"}
+	for _, day := range cooked.Days {
+		header = append(header, day)
+	}
+	out.Write(header)
+
+	//
+	for i, pubCode := range cooked.PubCodes {
+		dat := cooked.Data[i]
+		row := make([]string, len(header))
+		row[0] = pubCode
+		for j, cnt := range dat {
+			row[1+j] = strconv.Itoa(cnt)
+		}
+		out.Write(row)
+	}
+	out.Flush()
+	return out.Error()
 }
